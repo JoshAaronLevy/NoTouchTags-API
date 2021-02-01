4bf7d81ddbaa6667f723fbe0abcd988ddaa69aa8
Josh Levy <joshaaronlevy@gmail.com>
2021-01-31 23:29:52 -0700

    "Updated allowed headers to include firebase url. Deploying to Heroku"

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+// const allowedDomains = ['http://localhost:4200', 'https://tags-dashboard.web.app'];
 const port = parseInt(process.env.PORT || 3000);
 const Parse = require('parse/node');
 
 Parse.initialize('w17nTT0TMtIjiH1qzS4r6zAGig6EOAHNpncny7VF', 'kBlbaoq8fd2KblWBNEApECYIEPiCF8CqZyWvXYK2');
 Parse.serverURL = 'https://parseapi.back4app.com';
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // bypass the requests with no origin (like curl requests, mobile apps, etc )
+//     if (!origin) return callback(null, true);
+
+//     if (allowedDomains.indexOf(origin) === -1) {
+//       var msg = `This site ${origin} does not have access. Only specific domains are allowed to access it.`;
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
@@ -13,7 +27,7 @@ app.use(function (req, res, next) {
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  res.setHeader('Access-Control-Allow-Origin', allowedOrigins);
+  // res.setHeader('Access-Control-Allow-Origin', allowedOrigins);
 
   // Request methods you wish to allow
   res.setHeader(
