ca7d5d9362b5494a1557976831d1477daa897b7c
Josh Levy <joshaaronlevy@gmail.com>
2021-01-31 23:01:48 -0700

    "Updated allowed headers to include firebase url. Deploying to Heroku"

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ Parse.serverURL = 'https://parseapi.back4app.com';
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200, https://tags-dashboard.web.app');
 
   // Request methods you wish to allow
   res.setHeader(
